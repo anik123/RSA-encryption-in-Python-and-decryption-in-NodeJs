37965ea47f2f47303dba7014ad91d88c559ac685
Anik Islam Abhi <anik123@users.noreply.github.com>
2018-12-06 14:04:13 +0900

    Update node-crypto.js

--- node-crypto.js
+++ node-crypto.js
@@ -7,24 +7,6 @@ function print(msg){
 
 const key = new NodeRSA({b: 1024});
 
-/*const text = 'Hello RSA!';
-const encrypted = key.encrypt(text, 'buffer');
-console.log('encrypted: ', encrypted);
-const decrypted = key.decrypt(encrypted, 'utf8');
-console.log('decrypted: ', decrypted);*/
-
-const publicDer = key.exportKey('pkcs8-public-pem');
-const privateDer = key.exportKey('pkcs1-pem');
-
-const text = 'Hello RSA!';
-
-const publicKey = new NodeRSA(publicDer);
-const priKey = new NodeRSA(privateDer);
-//console.log(publicKey);
-
-const encrypted = publicKey.encrypt(text, 'buffer');
-const decrypt = priKey.decrypt(encrypted, 'utf8');
-
 var contents = fs.readFileSync("/home/anik/Desktop/Tutorial-Multi Node/Code/encrypted.txt");
 print(contents);
 
@@ -41,7 +23,3 @@ print(testPri);
 const decrypt1 = testPri.decrypt(contents, 'utf8');
 
 print(decrypt1)
-
-//console.log(publicDer);
-//console.log(encrypted);
-//console.log(decrypt);
